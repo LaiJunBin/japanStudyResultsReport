54b2460035dcb002bc9c48e7b4c6b22aee19e17e
xyz607xx <xyz607xx@gmail.com>
2018-05-28 00:35:51 +0800

    無圖片不觸發layout

--- resources/views/photoDay.blade.php
+++ resources/views/photoDay.blade.php
@@ -211,42 +211,44 @@
 
 
         function layout() {
-            //圖片最小的寬度
-            var minWidth = 340;
-            //紀錄高度的陣列
-            var height = [];
-            var li = document.querySelectorAll('#myUl li');
-            //畫面寬度等於#myUl的寬度
-            var mainWidth = ul.offsetWidth;
-            //每個li的寬度都一樣，所以取第一個就好
-            var liWidth = li[0].offsetWidth;
-            //計算現在的版面一列可以塞下幾張圖
-            var count = Math.floor(mainWidth / minWidth);
-            //之後推算一個li的寬度可以多寬剛好填滿版面，至少不要留白太多(畫面小的情況下)
-            var setLiWidth = Math.floor(mainWidth / count);
-            for (var i = 0; i < li.length; i++) {
-                li[i].style.width = setLiWidth + 'px';
-                li[i].children[0].style.width = setLiWidth + 'px';
-                //第一列
-                if (i < count) {
-                    li[i].style.left = setLiWidth * i + 'px';
-                    li[i].style.top = 0 + 'px';
-                    height[i] = li[i].offsetHeight;
-                } else {
-                    //如果不是第一列就找出最小的高度往下疊
-                    var min = Math.min.apply(null, height);
-                    var index = height.indexOf(min);
-                    li[i].style.left = index * setLiWidth + 'px';
-                    li[i].style.top = height[index] + 'px';
-                    height[index] += li[i].offsetHeight;
-                }
+            if(images.length>0){
+                //圖片最小的寬度
+                var minWidth = 340;
+                //紀錄高度的陣列
+                var height = [];
+                var li = document.querySelectorAll('#myUl li');
+                //畫面寬度等於#myUl的寬度
+                var mainWidth = ul.offsetWidth;
+                //每個li的寬度都一樣，所以取第一個就好
+                var liWidth = li[0].offsetWidth;
+                //計算現在的版面一列可以塞下幾張圖
+                var count = Math.floor(mainWidth / minWidth);
+                //之後推算一個li的寬度可以多寬剛好填滿版面，至少不要留白太多(畫面小的情況下)
+                var setLiWidth = Math.floor(mainWidth / count);
+                for (var i = 0; i < li.length; i++) {
+                    li[i].style.width = setLiWidth + 'px';
+                    li[i].children[0].style.width = setLiWidth + 'px';
+                    //第一列
+                    if (i < count) {
+                        li[i].style.left = setLiWidth * i + 'px';
+                        li[i].style.top = 0 + 'px';
+                        height[i] = li[i].offsetHeight;
+                    } else {
+                        //如果不是第一列就找出最小的高度往下疊
+                        var min = Math.min.apply(null, height);
+                        var index = height.indexOf(min);
+                        li[i].style.left = index * setLiWidth + 'px';
+                        li[i].style.top = height[index] + 'px';
+                        height[index] += li[i].offsetHeight;
+                    }
 
+                }
+                //把#myUl的高度設定的與li一樣高，最後底部的位置比較好定位。
+                ul.style.height = Math.max.apply(null, height) + 'px';
+                //如果到底部了
+                if (document.querySelector("#bottom") != null)
+                    document.querySelector("#bottom").style.top = Math.max.apply(null, height) + 'px';
             }
-            //把#myUl的高度設定的與li一樣高，最後底部的位置比較好定位。
-            ul.style.height = Math.max.apply(null, height) + 'px';
-            //如果到底部了
-            if (document.querySelector("#bottom") != null)
-                document.querySelector("#bottom").style.top = Math.max.apply(null, height) + 'px';
         }
         var adjust = setInterval(function(){
             var n = 0;
